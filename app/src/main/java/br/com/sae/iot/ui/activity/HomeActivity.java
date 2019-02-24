f7761d9c42044fbe676ef56a668a5985a439a283
Cássio Paixão <cassiopaixaoti@gmail.com>
2019-02-24 08:25:01 -0300

    custom bottom navigation

--- app/src/main/java/br/com/sae/iot/ui/activity/HomeActivity.java
+++ app/src/main/java/br/com/sae/iot/ui/activity/HomeActivity.java
@@ -1,15 +1,42 @@
 package br.com.sae.iot.ui.activity;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import br.com.sae.iot.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+//
+//        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_id);
+//        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        int id = menuItem.getItemId();
+        switch (id) {
+            case R.id.home_id:
+                return true;
+
+            case R.id.area_id:
+                return true;
+
+            case R.id.product_id:
+                return true;
+
+        }
+        return false;
     }
 }
--- app/src/main/java/br/com/sae/iot/ui/activity/MainActivity.java
+++ app/src/main/java/br/com/sae/iot/ui/activity/MainActivity.java
@@ -1,7 +1,11 @@
 package br.com.sae.iot.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -14,20 +18,19 @@ import br.com.sae.iot.R;
  * @author cassiopaixao
  */
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener  {
 
     private ImageView bgapp, clover;
     private LinearLayout textsplash, texthome, menus;
     private Animation frombottom;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         frombottom = AnimationUtils.loadAnimation(this, R.anim.frombottom);
-
 
         bgapp = (ImageView) findViewById(R.id.bgapp);
         clover = (ImageView) findViewById(R.id.clover);
@@ -42,19 +45,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         texthome.startAnimation(frombottom);
         menus.startAnimation(frombottom);
 
-        bgapp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_id);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
     }
+
+    public void goToHome(View view){
+        startActivity(new Intent(this, HomeActivity.class));
+    }
+
 
     @Override
-    public void onClick(View v) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
+        int id = menuItem.getItemId();
+        switch (id) {
+            case R.id.home_id:
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+
+            case R.id.area_id:
+                startActivity(new Intent(this, HomeActivity.class));
+                return true;
+
+            case R.id.product_id:
+                return true;
+
+        }
+        return false;
     }
+
 }
 
