2450388152ff682c35ca3b3374e22d18135c3ecb
Cássio Paixão <cassiopaixaoti@gmail.com>
2019-03-01 10:32:44 -0300

    fix open camera for capture image ou get multimidia in storage.

--- app/src/main/java/br/com/sae/iot/ui/fragments/problem/FormProblemFragment.java
+++ app/src/main/java/br/com/sae/iot/ui/fragments/problem/FormProblemFragment.java
@@ -1,15 +1,20 @@
 package br.com.sae.iot.ui.fragments.problem;
 
+import android.Manifest;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,8 +27,13 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import br.com.sae.iot.R;
 import br.com.sae.iot.dao.IndustryAreaDAO;
@@ -34,6 +44,7 @@ import br.com.sae.iot.model.Industry;
 import br.com.sae.iot.model.Problem;
 import br.com.sae.iot.utils.FormValidator;
 
+import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
 
 public class FormProblemFragment extends Fragment implements View.OnClickListener {
@@ -47,7 +58,10 @@ public class FormProblemFragment extends Fragment implements View.OnClickListene
     private Industry industry;
     private Problem problem;
     private ImageView imageView;
-    private static final int REQUEST_IMAGE = 101;
+
+    public static final int REQUEST_IMAGE = 100;
+    public static final int REQUEST_PERMISSION = 200;
+    private String imageFilePath = "";
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Nullable
@@ -58,17 +72,14 @@ public class FormProblemFragment extends Fragment implements View.OnClickListene
         industry = new Industry();
         problem = new Problem();
         spinnerArea = (Spinner) mView.findViewById(R.id.selected_area);
-        imageView = (ImageView) mView.findViewById(R.id.img_view_cam);
-        mButtonCam = (Button) mView.findViewById(R.id.take_photo);
+        imageView = (ImageView) mView.findViewById(R.id.image_problem);
+        mButtonCam = (Button) mView.findViewById(R.id.button_take_photo);
         mButtonCam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
-                    startActivityForResult(intent, REQUEST_IMAGE);
-                }
+                verifyPermission();
             }
+
         });
 
         spinnerArea.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -105,12 +116,43 @@ public class FormProblemFragment extends Fragment implements View.OnClickListene
         return mView;
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE && resultCode == RESULT_OK) {
-            Bundle bundle = data.getExtras();
-            Bitmap bitmap = (Bitmap) bundle.get("data");
-            imageView.setImageBitmap(bitmap);
+    private void verifyPermission() {
+        ArrayList<String> permissions = new ArrayList<>();
+        if (ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+        if (ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.CAMERA);
+        }
+        if (permissions.isEmpty()) {
+            openCameraIntent();
+        } else {
+            String[] permissoesForRequest = new String[permissions.size()];
+            permissions.toArray(permissoesForRequest);
+            ActivityCompat.requestPermissions(getActivity(),
+                    permissoesForRequest,
+                    0);
+        }
+    }
+
+    private void openCameraIntent() {
+        Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (pictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+            Uri photoUri = FileProvider.getUriForFile(mView.getContext(), "br.com.sae.iot.provider", photoFile);
+            pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            startActivityForResult(pictureIntent, REQUEST_IMAGE);
         }
     }
 
@@ -216,6 +258,43 @@ public class FormProblemFragment extends Fragment implements View.OnClickListene
         }
 
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_PERMISSION && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(mView.getContext(), "Thanks for granting Permission", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE) {
+            if (resultCode == RESULT_OK) {
+                imageView.setImageURI(Uri.parse(imageFilePath));
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(mView.getContext(), "You cancelled the operation", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "IMG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        imageFilePath = image.getAbsolutePath();
+
+        return image;
+    }
+
 
 }
 
